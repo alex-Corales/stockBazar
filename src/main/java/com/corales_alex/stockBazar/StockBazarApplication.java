1196e3290c6e3463e187dafde2edb052a89e3e8f
Alex Nahuel Corales <alexcorales21@gmail.com>
2024-11-16 16:42:55 -0300

    Initial commit

---
+++ src/main/java/com/corales_alex/stockBazar/StockBazarApplication.java
@@ -0,0 +1,13 @@
+package com.corales_alex.stockBazar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockBazarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockBazarApplication.class, args);
+	}
+
+}
