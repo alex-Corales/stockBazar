5b48e60d2f739e1fef7a85cec4c31e58e4ad3011
Alex Nahuel Corales <alexcorales21@gmail.com>
2024-11-16 18:46:56 -0300

    Update VentaModel.java
    Agregue las anotations @OneToOne y @OneToMany

--- src/main/java/com/corales_alex/stockBazar/models/VentaModel.java
+++ src/main/java/com/corales_alex/stockBazar/models/VentaModel.java
@@ -3,11 +3,12 @@ package com.corales_alex.stockBazar.models;
 import java.time.LocalDate;
 import java.util.List;
 
-import jakarta.annotation.Generated;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -20,7 +21,9 @@ public class VentaModel {
     private Long codigoVenta;
     private LocalDate fechaVenta;
     private Double total;
+    @OneToMany
     private List<ProductoModel> listaProductos;
+    @OneToOne
     private ClienteModel cliente;
 
     public VentaModel(){}
