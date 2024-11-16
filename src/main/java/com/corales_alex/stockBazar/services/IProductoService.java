7f7e818604d7280446afd45ddd2721f4dd7f6289
Alex Nahuel Corales <alexcorales21@gmail.com>
2024-11-16 18:55:50 -0300

    Update IProductoService.java

--- src/main/java/com/corales_alex/stockBazar/services/IProductoService.java
+++ src/main/java/com/corales_alex/stockBazar/services/IProductoService.java
@@ -1,9 +1,6 @@
 package com.corales_alex.stockBazar.services;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.corales_alex.stockBazar.models.ProductoModel;
-
-public interface IProductoService extends JpaRepository<ProductoModel, Long>{
+public interface IProductoService{
     
 }
 
