80ddb887bb10f686dfc99b0b3dc780392aa653d6
Alex Nahuel Corales <alexcorales21@gmail.com>
2024-11-17 22:15:23 -0300

    Agrego metodos estaticos a interface ClienteService

---
+++ src/main/java/com/corales_alex/stockBazar/services/IClienteService.java
@@ -0,0 +1,13 @@
+package com.corales_alex.stockBazar.services;
+
+import java.util.List;
+import com.corales_alex.stockBazar.models.ClienteModel;
+
+public interface IClienteService {
+    public void saveCliente(ClienteModel cliente);
+    public List<ClienteModel> getAllCliente();
+    public ClienteModel getCliente(Long idCliente);
+    public void deleteCliente(Long idCliente);
+    public void updateCliente(Long idCliente);
+}
+
