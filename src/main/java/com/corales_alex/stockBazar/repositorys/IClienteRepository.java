a63de1184bcd8c4ee38af5c6fed3214761932af5
Alex Nahuel Corales <alexcorales21@gmail.com>
2024-11-17 22:09:06 -0300

    Create IClienteRepository.java

---
+++ src/main/java/com/corales_alex/stockBazar/repositorys/IClienteRepository.java
@@ -0,0 +1,12 @@
+package com.corales_alex.stockBazar.repositorys;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.corales_alex.stockBazar.models.ClienteModel;
+
+@Repository
+public interface IClienteRepository extends JpaRepository<ClienteModel, Long>{
+
+}
+
+
