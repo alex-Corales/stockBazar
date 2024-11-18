3b5eb1c4c3274ab64a77d26b01efce28fe6eaa6a
Alex Nahuel Corales <alexcorales21@gmail.com>
2024-11-17 22:06:32 -0300

    Update @requestMapping ProductController

--- src/main/java/com/corales_alex/stockBazar/controllers/ProductoController.java
+++ src/main/java/com/corales_alex/stockBazar/controllers/ProductoController.java
@@ -8,39 +8,41 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.corales_alex.stockBazar.models.ProductoModel;
 import com.corales_alex.stockBazar.services.IProductoService;
 
 @RestController
+@RequestMapping("/productos")
 public class ProductoController {
     
     @Autowired
     private IProductoService iProductoService;
 
-    @PostMapping("/productos/crear")
+    @PostMapping("/crear")
     public String getProducto(@RequestBody ProductoModel producto){
         iProductoService.saveProducto(producto);
         return "El producto se creo correctamente";
     }
 
-    @GetMapping("/productos")
+    @GetMapping("/")
     public List<ProductoModel> getAllProductos(){
         return iProductoService.getAllProductos();
     }
 
-    @GetMapping("/productos/{codigoProducto}")
+    @GetMapping("/{codigoProducto}")
     public ProductoModel getProducto(@PathVariable Long codigoProducto){
         return iProductoService.getProducto(codigoProducto);
     }
 
-    @DeleteMapping("/productos/eliminar/{codigoProducto}")
+    @DeleteMapping("/eliminar/{codigoProducto}")
     public String deteleProducto(@PathVariable Long codigoProducto){
         iProductoService.deteleProducto(codigoProducto);
         return "El producto se elimino correctamente";
     }
 
-    @PutMapping("/productos/editar/{codigoProducto}")
+    @PutMapping("/editar/{codigoProducto}")
     public String updateProducto(@PathVariable Long codigoProducto, @RequestBody ProductoModel producto){
         iProductoService.updateProducto(codigoProducto, producto);
         return "El producto se modifico correctamente";
